adf6c55fda19d553284c299a4a3f7d41db108360
Rohan <rohan.farzana@mable.com.au>
2025-01-20 15:23:28 +1100

    set up Employee model

---
+++ app/models/employee.rb
@@ -0,0 +1,5 @@
+class Employee < ApplicationRecord
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
+end
