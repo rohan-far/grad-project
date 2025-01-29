399bfc8f5283b256420131ae87279ed8f0bd276b
Rohan <rohan.farzana@mable.com.au>
2025-01-29 14:06:31 +1100

    list of employees query are saved in graphQL documentation

---
+++ app/graphql/types/employee_type.rb
@@ -0,0 +1,15 @@
+class Types::EmployeeType < Types::BaseObject
+
+    field :id, ID, null: false
+    field :first_name, String, null: false
+    field :last_name, String, null: false
+    field :email, String, null: false
+    field :yearly_salary, Float, null: false
+    field :department, String, null: false
+
+    field :full_name, String, null: false
+
+    def full_name
+        "#{object.first_name} #{object.last_name}"
+    end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -22,10 +22,10 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :employees, [Types::EmployeeType], null: false
+
+    def employees
+      Employee.all
     end
   end
 end
