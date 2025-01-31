86cdd75a1406319619b6b620db1961ed1be1e2e0
Rohan <rohan.farzana@mable.com.au>
2025-01-31 20:52:55 +1100

    refector validaton role in sign_up

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,25 +1,13 @@
 class Employee < ApplicationRecord
 
-  before_validation :set_defaults, on: :create
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
     belongs_to :department
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    validates :department, presence: true
-    validates :yearly_salary, presence: true, numericality: { greater_than: 0 }
-
-    private
-
-    def set_defaults
-      self.yearly_salary ||= 1.0
-      self.first_name ||= "New"
-      self.last_name ||= "Employee"
-      self.yearly_salary ||= 0.0
-      self.department ||= Department.first
-    end
+    validates :first_name, presence: true, unless: -> { new_record }
+    validates :last_name, presence: true, unless: -> { new_record }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { new_record }
+    validates :department, presence: true, unless: -> { new_record }
+    validates :yearly_salary, presence: true, numericality: { greater_than: 0 }, unless: -> { new_record }
 end
