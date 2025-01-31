2540222ead02b3a0fef3c5426f46dc92d2a2def2
Rohan <rohan.farzana@mable.com.au>
2025-01-31 23:02:51 +1100

    setup cancancan

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,11 +6,11 @@ class Employee < ApplicationRecord
     belongs_to :department
 
     enum role: { default: "default", admin: "admin" }, _prefix: true
-    
-    validates :role, inclusion: { in: roles.keys } 
-    validates :first_name, presence: true, unless: -> { new_record }
-    validates :last_name, presence: true, unless: -> { new_record }
-    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { new_record }
-    validates :department, presence: true, unless: -> { new_record }
-    validates :yearly_salary, presence: true, numericality: { greater_than: 0 }, unless: -> { new_record }
+
+    validates :role, inclusion: { in: roles.keys },  unless: -> { @new_record }
+    validates :first_name, presence: true, unless: -> { @new_record }
+    validates :last_name, presence: true, unless: -> { @new_record }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: -> { @new_record }
+    validates :department, presence: true, unless: -> { @new_record }
+    validates :yearly_salary, presence: true, numericality: { greater_than: 0 }, unless: -> { @new_record }
 end
