a807c2439ce3c7b75edacecb4d12873ccfb7f2d9
Rohan <rohan.farzana@mable.com.au>
2025-02-03 12:51:27 +1100

    fix some tests and added new test for employee role

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Employee, type: :model do
-    it "is valid with valid attributes" do
-        employee = FactoryBot.build(:employee)
-        expect(employee).to be_valid
+RSpec.describe Employee, type: :model do                
+    let(:department) {FactoryBot.create(:department)}
+
+    it "employee is valid" do
+        employee = FactoryBot.create(:employee, department: department)
+        expect(employee.department.name).to eq("Engineering")
     end
     it "has invalid first_name" do
-        employee = FactoryBot.build(:employee, first_name:nil)
+        employee = FactoryBot.create(:employee, first_name:nil)
         expect(employee).not_to be_valid
     end
     it "has invalid last_name" do
-        employee = FactoryBot.build(:employee, last_name:nil)
+        employee = FactoryBot.create(:employee, last_name:nil)
         expect(employee).not_to be_valid
     end
     it "has invalid email" do
@@ -18,22 +20,32 @@ RSpec.describe Employee, type: :model do
         expect(employee).not_to be_valid
     end
     it "has invalid with non-unique email" do
-        FactoryBot.create(:employee, email: "rohan@1.com")
-        employee = FactoryBot.build(:employee, email: "rohan@1.com")
-        expect(employee).not_to be_valid
+        employee1 = FactoryBot.create(:employee, email: "rohan@1.com")
+        employee2 = FactoryBot.build(:employee, email: "rohan@1.com")
+        expect(employee2).not_to be_valid
     end
     it "is invalid with an improperly formatted email" do
         employee = FactoryBot.build(:employee, email: "rohan.fardd.com")
         expect(employee).not_to be_valid
-      end
+    end
     
-      it "is invalid without a department" do
+    it "is invalid without a department" do
         employee = FactoryBot.build(:employee, department: nil)
         expect(employee).not_to be_valid
-      end
-    
-      it "is invalid without a yearly_salary greater than 0" do
-        employee = FactoryBot.build(:employee, yearly_salary: 0)
+    end
+
+    it "is invalid without a yearly_salary greater than 0" do
+        employee = FactoryBot.create(:employee, yearly_salary: 0)
         expect(employee).not_to be_valid
-      end
+    end
+
+    it "check the role for new sign_uo employee" do
+        employee = FactoryBot.create(:employee, role: nil)
+        employee.valid?
+    expect(employee.role).to eq("default")
+    end
+    it "allows set a role admin" do
+        employee = FactoryBot.build(:employee, role: 'admin')
+        expect(employee.role).to eq('admin')
+    end
 end
